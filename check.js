04d784e191ac3075c4119455a0b4f7a4e1daf624
Bruna Guimarães <103650184+bruna-gguimaraes@users.noreply.github.com>
2025-03-28 02:30:20 -0300

    Update check.js

--- check.js
+++ check.js
@@ -1,14 +1,20 @@
-function check() {
-    if ( document.forms[0].elements[0].checked == true && document.forms[0].elements[1].checked == true && document.forms[0].elements[2].checked == true ) {
-      if ( ! $('.wrapper').hasClass('throb')) {
-          // Do things on Nav Close
-          $('.wrapper').addClass('throb');
-      } 
+unction check() {
+    // Selecionando os checkboxes e o elemento .wrapper
+    const checkboxes = document.forms[0].elements;
+    const wrapper = $('.wrapper');
+
+    // Verificando se todos os três checkboxes estão marcados
+    const allChecked = Array.from(checkboxes).slice(0, 3).every(checkbox => checkbox.checked);
+
+    // Se todos estiverem marcados e a classe não estiver presente, adicione a classe 'throb'
+    if (allChecked) {
+        if (!wrapper.hasClass('throb')) {
+            wrapper.addClass('throb');
+        }
     } else {
-      if ($('.wrapper').hasClass('throb')) {
-          // Do things on Nav Close
-          $('.wrapper').removeClass('throb');
-      }
+        // Se qualquer um não estiver marcado e a classe estiver presente, remova a classe 'throb'
+        if (wrapper.hasClass('throb')) {
+            wrapper.removeClass('throb');
+        }
     }
-   }
    
